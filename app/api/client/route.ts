985d185d52f937a904fdedc33844518db6b16889
bertemesdaniel1-svg <bertemesdaniel1@gmail.com>
2026-03-11 23:00:18 +0100

    Update route.ts

--- app/api/client/route.ts
+++ app/api/client/route.ts
@@ -1,1 +1,27 @@
+import { NextResponse } from "next/server";
+import { sql } from "@/lib/db";
 
+export async function GET() {
+  try {
+
+    const result = await sql`
+      SELECT id, first_name, last_name, email, client_secret, subscription_status
+      FROM clients
+      LIMIT 1
+    `;
+
+    const client = result[0];
+
+    return NextResponse.json({
+      success: true,
+      client
+    });
+
+  } catch (err) {
+
+    return NextResponse.json({
+      success: false
+    });
+
+  }
+}
