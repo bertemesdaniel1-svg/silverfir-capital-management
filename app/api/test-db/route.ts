44bb171a6298304b212b10a3297d82b861b5a6b8
bertemesdaniel1-svg <bertemesdaniel1@gmail.com>
2026-03-11 21:43:26 +0100

    Update route.ts

--- app/api/test-db/route.ts
+++ app/api/test-db/route.ts
@@ -1,1 +1,15 @@
+import { neon } from "@neondatabase/serverless";
+import { NextResponse } from "next/server";
 
+const sql = neon(process.env.DATABASE_URL!);
+
+export async function GET() {
+
+  const result = await sql`SELECT NOW()`;
+
+  return NextResponse.json({
+    connected: true,
+    time: result[0]
+  });
+
+}
