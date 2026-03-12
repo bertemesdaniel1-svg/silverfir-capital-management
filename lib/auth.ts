c665acf89b0e5710a78e8caac2164fa5a2123d58
bertemesdaniel1-svg <bertemesdaniel1@gmail.com>
2026-03-12 08:59:02 +0100

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -23,7 +23,6 @@ export async function createSession(clientId: number) {
 }
 
 export async function getClientFromSessionToken(token: string) {
-
   const result = await sql`
     SELECT
       c.id,
@@ -31,7 +30,9 @@ export async function getClientFromSessionToken(token: string) {
       c.last_name,
       c.email,
       c.client_secret,
-      c.subscription_status
+      c.subscription_status,
+      c.payment_status,
+      c.is_approved
     FROM sessions s
     JOIN clients c ON c.id = s.client_id
     WHERE s.session_token = ${token}
@@ -40,7 +41,6 @@ export async function getClientFromSessionToken(token: string) {
   `;
 
   return result[0] ?? null;
-
 }
 
 export async function deleteSession(token: string) {
